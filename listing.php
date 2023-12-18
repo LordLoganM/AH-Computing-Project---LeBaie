61b37f91e00ef961e001b510222cc2fc797c01bc
LordLoganM <logan.c.muir@gmail.com>
2023-12-18 23:18:50 +0000

    add a new column
    one in the form for the category of items being sold and a new column in db for condition of items

--- listing.php
+++ listing.php
@@ -6,11 +6,16 @@
 session_start();
 
  
+$userName = $_GET["productname"];
+$firstName = $_GET["condition"];
+$surName = $_GET["price"];
+$dateOfBirth = $_GET["message"];
+$userPassword = $_GET["image"];
 
-            $servername="localhost";
-            $username="root";
-            $password="";
-            $dbname="leBaieBase";
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="leBaieBase";
         
         $conn=mysqli_connect($servername, $username, $password, $dbname);
     
@@ -19,19 +24,7 @@ session_start();
     die("Connection failed: " . mysqli_connect_error()); } 
 
 
-    
-$result= mysqli_query($conn, "SELECT `username` FROM `registration` WHERE `username`='".$userName."'");
-
-  if (mysqli_num_rows($result) >0)
-  {
-      echo "<p>Username already exists</p>";
-      $redirect1 = TRUE;
-      if ($redirect1) {
-         header('Location: http://localhost/implementation/failedReg.html');
-         die();}}
-
-else{
-    $sql = "INSERT INTO `registration`  (`username`,`firstname`,`surname`,`DOB`,`password`) 
+    $sql = "INSERT INTO `products`  (`productName`,`productCa`,`surname`,`DOB`,`password`) 
     VALUES('$userName','$firstName','$surName','$dateOfBirth','$userPassword')";
   
   if(mysqli_query($conn, $sql)) {
@@ -43,11 +36,6 @@ if ($redirect) {
    die();}
 
 }
-
-$_SESSION["userName"]= $userName;
-$_SESSION["firstname"] = $firstName; 
-$_SESSION["password"] = $userPassword; 
-
 ?>
 
 
