1836c899fbb325cd2a527fcb9da4174fe0d212ef
LordLoganM <logan.c.muir@gmail.com>
2024-01-13 02:24:16 +0000

    cat is undeclared

--- listing.php
+++ listing.php
@@ -7,6 +7,7 @@ session_start();
 
  
 $userProduct = $_GET["productname"];
+$itemCategory = $_GET["category"];
 $condition = $_GET["condition"];
 $price = $_GET["price"];
 $message = $_GET["message"];
@@ -24,17 +25,24 @@ $dbname="leBaieBase";
     die("Connection failed: " . mysqli_connect_error()); } 
 
 
-    $sql = "INSERT INTO `products`  (`productName`,`productCategory`,`price`,`description`,`image`) 
-    VALUES('$userProduct','$condition','$surName','$price', '$message', '$image')";
+    $sql = "INSERT INTO `products`  (`productName`,`productCategory`,`condition`,`price`,`description`,`image`) 
+    VALUES('$userProduct','$itemCategory','$condition','$price', '$message', '$image')";
   
   if(mysqli_query($conn, $sql)) {
     echo "Product Listed";} 
 
     $redirect = TRUE;
 if ($redirect) {
-   header('Location: http://localhost/implementation/RegisteredHome.html');
+   header('Location: http://localhost/implementation/RegisteredHome.php');
    die();}
 
+  
+
+   else{
+       
+       echo "<p>Error Registering Account</p>";
+       }
+       
 
 ?>
 
